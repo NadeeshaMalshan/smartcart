500fb5328c1f8afe9e755ca396f668d9a8af8f68
Nadeesha Malshan <malshannandanayaka@gmail.com>
2025-09-27 14:45:13 +0530

    Initial commit

---
+++ src/test/java/com/group35/smartcart/SmartcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group35.smartcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
